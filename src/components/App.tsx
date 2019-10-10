09f52f32c21960568715949e714669d4439467a5
stephan-kashkarov <kashkarovwork@gmail.com>
2019-10-10 13:43:01 +1100

    finished navbar lol

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
-import logo from './imgs/logo.svg';
-// import Navbar from './navbar';
+import Navigation from './navigation';
 import './styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,16 +8,7 @@ class App extends Component {
 	render () {
 		return (
 			<div className="App">
-				<Navbar bg="dark" variant="dark">
-					<Container>
-						<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#features">Features</Nav.Link>
-							<Nav.Link href="#pricing">Pricing</Nav.Link>
-						</Nav>
-					</Container>
-				</Navbar>
+				<Navigation />
 			</div>
 		);
 	}
